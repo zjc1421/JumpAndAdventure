7586d4ade1ce6e94a2cf3f8934eea76e85d72dda
LushanWang <lushanw@uw.edu>
2024-04-30 20:24:39 -0700

    update

--- ultrasonic_sensor_example/ultrasonic_sensor_example.ino
+++ ultrasonic_sensor_example/ultrasonic_sensor_example.ino
@@ -1,22 +1,29 @@
 #include "SR04.h"
 #define TRIG_PIN 12
 #define ECHO_PIN 11
+
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-int initial_distance;
-long a;
+int act_dist;
+int sum_dist = 0;
+int avg_dist = 0;
+int ref_dist = 0;
+int n = 0;
 
 void setup() {
-   Serial.begin(9600);
+  Serial.begin (9600);
+  while (millis() < 5000) {
+    act_dist = sr04.Distance();
+    sum_dist = sum_dist + act_dist;
+    n++;
+    delay(200);
+  }
+  avg_dist = sum_dist / n;
+  Serial.println("Done Calibration.");
 }
 
 void loop() {
-   a=sr04.Distance();
-   Serial.println(a);
-   delay(5);
-   if (a >= 10) {
-    Serial.println(1);
-   } else {
-    Serial.println(0);
-   }
-
+  act_dist = sr04.Distance();
+  ref_dist = act_dist - avg_dist;
+  Serial.println(ref_dist);
+  delay(500);
 }
